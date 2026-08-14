042340c394432876817dd88534ad25a0538bc544
Edgar Barco <ebarcoc@miumg.edu.gt>
2026-08-13 23:20:41 -0600

    feat: add toggleTarea and filterTareas for completed/pending views

--- Sesion-04-JavaScript-Parte-1/src/app.js
+++ Sesion-04-JavaScript-Parte-1/src/app.js
@@ -64,8 +64,12 @@ export function eliminarTarea(id) {
  * @returns {boolean}
  */
 export function toggleTarea(id) {
-    // TODO: recorrer `tareas` y cambiar `completada` de la que coincida.
-    // Devuelve true si la encontró.
+    const tarea = tareas.find((t) => t.id === id);
+    if (!tarea) {
+        return false;
+    }
+    tarea.completada = !tarea.completada;
+    return true;
 }
 
 /**
@@ -74,7 +78,13 @@ export function toggleTarea(id) {
  * @returns {Array}
  */
 export function filtrarTareas(filtro) {
-    // TODO: implementar la lógica de filtrado.
+    if (filtro === "pendientes") {
+        return tareas.filter((t) => !t.completada);
+    }
+    if (filtro === "completadas") {
+        return tareas.filter((t) => t.completada);
+    }
+    return tareas;
 }
 
 /**
